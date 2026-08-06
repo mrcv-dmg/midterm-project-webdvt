f0c13efa8ecacf6b5f1cb32af2608a16a26872d9
DANE <136380294+mrcv-dmg@users.noreply.github.com>
2026-08-06 23:53:53 +0800

    optimised a few code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+import Dashboard from "./critters/dashboard";
 
 function App() {
-  return (
-    <main className="flex min-h-screen items-center justify-center p-6">
-      <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle>Web Development Midterms</CardTitle>
-          <CardDescription>
-            Testing shadcn/ui
-          </CardDescription>
-        </CardHeader>
-
-        <CardContent>
-          <p>This is the content of the card.</p>
-        </CardContent>
-
-        <CardFooter>
-          <Button>Tester</Button>
-        </CardFooter>
-      </Card>
-    </main>
-  );
+  return <Dashboard />;
 }
 
 export default App;
---
+++ src/critters/dashboard.jsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+  return (
+    <h1>Budget Tracker Dashboard</h1>
+  );
+}
