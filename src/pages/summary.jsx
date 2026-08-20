1afd25fe898890808cf16713f56d5418769c7b6c
DANE <136380294+mrcv-dmg@users.noreply.github.com>
2026-08-20 13:50:02 +0800

    integrated the navbar to the summary page

--- src/pages/summary.jsx
+++ src/pages/summary.jsx
@@ -1,3 +1,10 @@
+import Navbar from "@/components/ui/navbar";
+
 export default function Summary() {
-  return <h1>Summary</h1>;
+  return (
+    <main className="container mx-auto max-w-6xl space-y-6 p-6">
+      <Navbar />
+      <h1>Summary</h1>
+    </main>
+  )
 }
